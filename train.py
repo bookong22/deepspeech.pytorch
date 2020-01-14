d7968935305ff84043d105ad179a427502a98401
chensd <chensd@silvrr.com>
2020-01-14 14:49:36 +0800

    try run

--- train.py
+++ train.py
@@ -254,6 +254,8 @@ if __name__ == '__main__':
             # print("output_sizes, target_sizes : ", output_sizes, target_sizes)
             # exit()  # test 20200114
             targets = targets.to(device)
+            output_sizes = output_sizes.to(device)
+            target_sizes = target_sizes.to(device)
             loss = criterion(float_out, targets, output_sizes, target_sizes)    # .to(device)
             loss = loss / inputs.size(0)  # average the loss by minibatch
 
